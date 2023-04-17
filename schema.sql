311437375391d5322792f36dd343d422682088e0
lyskz <lyski.zach@icloud.com>
2023-04-17 12:30:49 -0700

    added starting table data in sql

--- schema.sql
+++ schema.sql
@@ -6,15 +6,28 @@ USE company_db;
 
 -- must include department names and department ids
 CREATE TABLE departments_db (
-
+    dept_name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id)
 );
 
 -- must include job title, role id, the department that role belongs to, and the salary for that role
 CREATE TABLE roles_db (
-
+    job_title VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_id INT NOT NULL,
+    salary INT NOT NULL,
+    PRIMARY KEY(id) 
 );
 
 -- must include employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 CREATE TABLE employees_db (
-
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    dept_id INT NOT NULL,
+    salary INT NOT NULL,
+    managers_id INT NOT NULL,
+    PRIMARY KEY(id)
 );
