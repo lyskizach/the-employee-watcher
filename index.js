cd0b5b49867fbf0deb85822ffde997b1a3226388
lyskz <lyski.zach@icloud.com>
2023-04-17 12:10:49 -0700

    add notes for my work

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
+
+// function to view all departments
+function viewDept();
+
+// function to display all roles
+function viewRole();
+
+// function to view all employees
+function viewEmp();
+
+// function to add department
+function addDept();
+
+// functiom to add role
+function addRole();
+
+// function to add employee
+function addEmp();
+
+// function to update/change employee role
+function updateEmp();
--- schema.sql
+++ schema.sql
@@ -4,14 +4,17 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+-- must include department names and department ids
 CREATE TABLE departments_db (
 
 );
 
+-- must include job title, role id, the department that role belongs to, and the salary for that role
 CREATE TABLE roles_db (
 
 );
 
+-- must include employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 CREATE TABLE employees_db (
 
 );
