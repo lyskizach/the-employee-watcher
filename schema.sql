e43d1544d26edbc4efaf13f5e8dff01f467040a1
lyskz <lyski.zach@icloud.com>
2023-04-17 12:31:33 -0700

    fix typo on tables names

--- schema.sql
+++ schema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE company_db;
 USE company_db;
 
 -- must include department names and department ids
-CREATE TABLE departments_db (
+CREATE TABLE departments (
     dept_name VARCHAR(30) NOT NULL,
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id)
 );
 
 -- must include job title, role id, the department that role belongs to, and the salary for that role
-CREATE TABLE roles_db (
+CREATE TABLE roles (
     job_title VARCHAR(30) NOT NULL,
     id INT NOT NULL AUTO_INCREMENT,
     dept_id INT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE roles_db (
 );
 
 -- must include employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-CREATE TABLE employees_db (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
