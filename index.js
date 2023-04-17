a3e2f2244dec09486d2b5af76b089afca7206748
lyskz <lyski.zach@icloud.com>
2023-04-17 13:02:54 -0700

    add inquirer and responses

--- index.js
+++ index.js
@@ -2,6 +2,49 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
 
+function prompt() {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employees role'
+        ]
+    })
+    .then(function(response) {
+        switch(response.action) {
+            case 'View all departments':
+                viewDept();
+                break;
+            case 'View all roles':
+                viewRole();
+                break;
+            case 'View all employees':
+                viewEmp();
+                break;
+            case 'Add a department':
+                addDept();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmp();
+                break;
+            case 'Update an employee role':
+                updateEmpt();
+                break;
+        }
+    })
+};
+
 // function to view all departments
 function viewDept();
 
