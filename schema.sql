df3d427d8a62afb03ee87af455a2040b347d88e0
lyskz <lyski.zach@icloud.com>
2023-04-17 12:02:29 -0700

    add starter files

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE departments_db (
+
+);
+
+CREATE TABLE roles_db (
+
+);
+
+CREATE TABLE employees_db (
+
+);
